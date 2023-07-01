84ddfcac15a2a1e135d2a5023f530b2deccc31f2
AndreaDiFlorioDiRenzo <82772908+AndreaDiFlorioDiRenzo@users.noreply.github.com>
2023-07-01 16:11:21 +0200

    Andrea 01/07
    in OpenCamera.js : aggiunta funzione per visualizzare il risultato del face rec
    in main.py : modificato in modo da chiamare la funzione face rec e mandare il risultato a index.html

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import json
-import os
 from datetime import datetime
 
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, jsonify
 from google.cloud import storage
-from werkzeug.utils import secure_filename
 
 # requires pyopenssl
 
 app = Flask(__name__)
+
+
+# palceholder for real face recognition funtion
+def fecerec():
+    return "wellcome"
 
 
 # doorbell
@@ -37,26 +40,28 @@ def upload_data():
 @app.route('/upload', methods=['POST'])
 def upload():
     # check if the post request has the file part
+    if request.method == 'POST':
+        file = request.files['file']
 
-    file = request.files['file']
+        now = datetime.now()
+        print(now)
+        # formatto i nomi delle immagini come anno_mese_giorno__ora_minuti_secondi.png
+        current_time = now.strftime("%Y_%m_%d__%H_%M_%S")
+        fname = f'{current_time}.png'
+        # file.save(os.path.join(f'tmp/test_{current_time}.png')) non serve passare dal locale
+        # purtoppo ho dovuto mettere il link assoluto perche non funzionava con il relativo
+        # quindi probabilmente sui vostri pc non va
+        client = storage.Client.from_service_account_json("C:\\GitHub\\00-PCSC-Progetto-2023\\facerecognition2023"
+                                                          "-84f934357826.json")
+        bucket = client.bucket('doorbell-db')
+        source_file_name = fname
+        destination_blob_name = source_file_name
+        blob = bucket.blob(destination_blob_name)
 
-    now = datetime.now()
-    current_time = now.strftime("%H_%M_%S")
-    fname = f'{current_time}.png'
-    # file.save(os.path.join(f'tmp/test_{current_time}.png'))
-    # purtoppo ho dovuto mettere il link assoluto perche non funzionava con il relativo
-    # quindi probabilmente sui vostri pc non va
-    client = storage.Client.from_service_account_json("C:\\GitHub\\00-PCSC-Progetto-2023\\facerecognition2023"
-                                                      "-84f934357826.json")
-    bucket = client.bucket('doorbell-db')
-    source_file_name = fname
-    destination_blob_name = source_file_name
-    blob = bucket.blob(destination_blob_name)
-
-    # blob.upload_from_filename(os.path.join('tmp/',fname))
-
-    blob.upload_from_string(file.read(), content_type=file.content_type)
-    return 'saved'
+        blob.upload_from_string(file.read(), content_type=file.content_type)
+        res = fecerec()
+        # return jsonify(result=res)
+        return res
 
 
 if __name__ == '__main__':
--- static/src/js/OpenCamera.js
+++ static/src/js/OpenCamera.js
@@ -1,36 +1,41 @@
-navigator.mediaDevices.getUserMedia({video: true}).then(gotMedia).catch(error =>
-    console.error('getUserMedia() error:', error));
+function openCamera() {
+    navigator.mediaDevices.getUserMedia({video: true}).then(gotMedia).catch(error =>
+        console.error('getUserMedia() error:', error));
 
-function gotMedia(mediaStream) {
-    const mediaStreamTrack = mediaStream.getVideoTracks()[0];
-    const imageCapture = new ImageCapture(mediaStreamTrack);
+    function gotMedia(mediaStream) {
+        const mediaStreamTrack = mediaStream.getVideoTracks()[0];
+        const imageCapture = new ImageCapture(mediaStreamTrack);
 
-    //console.log(imageCapture);
+        //console.log(imageCapture);
 
-    function capture() {
-        imageCapture.takePhoto().then(blob => {
-            img = document.getElementById("img")
-            img.src = URL.createObjectURL(blob);
+        function capture() {
+            imageCapture.takePhoto().then(blob => {
+                img = document.getElementById("img")
+                img.src = URL.createObjectURL(blob);
 
-            var fd = new FormData();
-            fd.append('file', blob, 'screenshot.png');
+                var fd = new FormData();
+                fd.append('file', blob, 'screenshot.png');
 
-            $.ajax({
-                type: 'POST',
-                url: '/upload',
-                data: fd,
-                processData: false,
-                contentType: false
-            }).done(function (data) {
-                console.log(data);
-            });
+                $.ajax({
+                    type: 'POST',
+                    url: '/upload',
+                    data: fd,
+                    processData: false,
+                    contentType: false
+                }).done(function (data) {
+                    $('#result').text(data);
+                        console.log(data);
+                });
 
-            img.onload = () => {
-                URL.revokeObjectURL(this.src);
-            }
-        }).catch(error => console.error('takePhoto() error:', error));
-        window.setTimeout(capture, 100000)
+                img.onload = () => {
+                    URL.revokeObjectURL(this.src);
+                }
+            }).catch(error => console.error('takePhoto() error:', error));
+            window.setTimeout(capture, 100000)
+        }
+
+        capture();
     }
-
-    capture()
 }
+
+
