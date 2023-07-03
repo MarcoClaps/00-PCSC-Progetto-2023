4897e712ba51f18955ac3036268c6caa989b5211
AndreaDiFlorioDiRenzo <82772908+AndreaDiFlorioDiRenzo@users.noreply.github.com>
2023-07-03 12:03:01 +0200

    rov

--- abs_path.py
+++ abs_path.py
@@ -1,2 +1,2 @@
-abs_firestore = ""
-abs_cstorage = ""
+abs_firestore = "C:\\GitHub\\00-PCSC-Progetto-2023\\facerecognition2023-58824e4fb3cc.json"
+abs_cstorage = "C:\\GitHub\\00-PCSC-Progetto-2023\\facerecognition2023-84f934357826.json"
