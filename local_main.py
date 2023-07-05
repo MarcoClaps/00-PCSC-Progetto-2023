33220d87018289dbf4a70b4ba5c16d99b9578fe3
MarcoClaps <marcoclaps@gmail.com>
2023-07-05 12:32:42 +0200

    Mosso index.css modifica referenza in index.html

--- local_main.py
+++ local_main.py
@@ -3,5 +3,5 @@ from FaceRecognition import FaceRecognition
 frec = FaceRecognition()
 
 frec.set_parameters("Prova.png")
-#frec.encode_known_faces()
+# frec.encode_known_faces()
 frec.recognize_faces()
